f23a4fb1624ca810f148a55e38e0d6e3ae8759ca
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-04-03 19:22:08 -0300

    Exemple Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { Button } from "./components/Button"
+
 export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </>
   )
 }
